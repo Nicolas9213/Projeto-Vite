b53d0b07b9b2659213b249c0dd1d60c1d4f3187c
afonso <afonsosilva>
2023-10-16 21:34:41 -0300

    aaaa

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -3,14 +3,12 @@ import './Section.css';
 function Section() {
     return (
         <section className='section'>
-        <h1>imagem
         <div className='card'>
             <p className='text1'>New Arrival</p>
             <h2 className='text2'>Discover Our New Collection</h2>
             <p className='text3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
             <button className='botao'>BUY NOW</button>
         </div>
-        </h1>
         
 
         </section>
