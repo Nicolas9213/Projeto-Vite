ed2db003c03a6002942f84c72557bcb7ce4a7d02
a <a>
2023-11-06 19:12:56 -0300

    Final

--- src/components/Section4/index.jsx
+++ src/components/Section4/index.jsx
@@ -8,10 +8,12 @@ function Section4 () {
                 <p>Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
                 <button className='botao3'>Explore More</button>
             </div>
-            <img src="./public/images/Image.png" alt="" />
-            <div className='content2'>
-                <img id="img1" src="/public/images/Rectangle 25.png" alt="" />
-                <img id="img2" src="/public/images/Indicator.png" alt="" />
+            <div className='imgs'>
+                <img id="img" src="./public/images/Image.png" alt="" />
+                <div className='content2'>
+                    <img id="img1" src="/public/images/Rectangle 25.png" alt="" />
+                    <img id="img2" src="/public/images/Indicator.png" alt="" />
+                </div>
             </div>
             
         </section>
