6a1512d9ad6ee41fa8e656cfdbe898c45d0c8770
Nicolas A. Gazaniga <nicolasgazaniga13@gmail.com>
2023-11-06 14:38:21 -0300

    06/11 - Continuação

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Header from './components/Header';
 import Section from './components/Section';
 import Section2 from './components/Section2';
-import ImageCard from './components/ImageCard';
 import Section3 from './components/Section3';
-import CardImage from './components/CardImage';
 import Section4 from './components/Section4';
 import Section5 from './components/Section5';
 import Footer from './components/Footer';
@@ -13,9 +11,7 @@ const App = () => {
     <Header/>
     <Section/>
     <Section2/>
-    <ImageCard/>
     <Section3/>
-    <CardImage/>
     <Section4/>
     <Section5/>
     <Footer/>
--- src/components/CardImage/index.jsx
+++ src/components/CardImage/index.jsx
@@ -4,11 +4,13 @@ function CardImage (props) {
     return (
         <div className='Card'>
             <img src={props.img} />
+            <div className='textCard'>
             <h3 className="title" >{props.title}</h3>
             <h4 className='description'>{props.description}</h4>
+            </div>
             <div className='prices'>
-            <h4 className='price'>{props.price}</h4>
-            <h4 className='price2'>{props.price2}</h4>
+                <h4 className='price'>{props.price}</h4>
+                <h4 className='price2'>{props.price2}</h4>
             </div>
         </div>            
     )
